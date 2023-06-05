ef0cfe15172a2843ae1221ecc4978a38ac823339
gorkavillar <gorka@gorkavillar.com>
2023-06-05 18:32:41 +0200

    tema-02 preparacion

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-const nombre = "Gorka";
-
-let edad = 35;
-
-// const saludo: string = "Hola " + nombre + " desde typescript"
-const saludo: string = `Hola ${nombre} desde typescript v4, y tu edad es ${edad}`;
-
-console.log(saludo);
